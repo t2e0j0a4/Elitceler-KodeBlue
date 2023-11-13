32cba734ec26c5f2eb2587f35e908fe0a3595ad2
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-11-13 16:19:27 +0530

    skeletons added

--- src/app/dashboard/livecases/(overwiew)/page.tsx
+++ src/app/dashboard/livecases/(overwiew)/page.tsx
@@ -26,6 +26,15 @@ const page = () => {
                 <Suspense fallback={<EachCaseCard/>}>
                     <LiveCaseCard age={28} caseID='abc123' caseType='Heart Stroke' paymentType='Credit Card' gender='Male' />
                 </Suspense>
+                <Suspense fallback={<EachCaseCard/>}>
+                    <LiveCaseCard age={28} caseID='abc123' caseType='Heart Stroke' paymentType='Credit Card' gender='Male' />
+                </Suspense>
+                <Suspense fallback={<EachCaseCard/>}>
+                    <LiveCaseCard age={28} caseID='abc123' caseType='Heart Stroke' paymentType='Credit Card' gender='Male' />
+                </Suspense>
+                <Suspense fallback={<EachCaseCard/>}>
+                    <LiveCaseCard age={28} caseID='abc123' caseType='Heart Stroke' paymentType='Credit Card' gender='Male' />
+                </Suspense>
             </section>
 
         </main>
