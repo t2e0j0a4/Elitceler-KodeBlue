e67d2e1490046cb50e7002ae311340efe5b2798c
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-11-15 21:28:57 +0530

    layout updated

---
+++ src/app/dashboard/livecases/[case]/Map.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./page.module.css";
+
+const Map = () => {
+  return (
+    <div className={styles.paramedic__map}>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11525.121488972181!2d78.96643550875214!3d16.010959768447595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb48c1241adb0e3%3A0x5c8ac521fa9f1f8c!2sNallamala%20Forest!5e1!3m2!1sen!2sin!4v1699970937012!5m2!1sen!2sin" style={{border: "0", width: '100%', height: 'auto', aspectRatio: '16/9.4', borderRadius: '12px' }} allowFullScreen loading="lazy"></iframe>
+    </div>
+  )
+}
+
+export default Map
--- src/app/dashboard/livecases/[case]/page.tsx
+++ src/app/dashboard/livecases/[case]/page.tsx
@@ -20,6 +20,9 @@ import { BsChatLeftText, BsHeartPulse, BsArrowDownShort, BsArrowUpShort, BsDropl
 // Skeletons
 import MapLoadSkeleton from '@/skeletons/cases/MapLoadSkeleton/MapLoadSkeleton';
 
+// Components
+import Map from './Map';
+
 // Updated as a Dynamic Metadata if needed later, but as of now just a static Metadata
 export const metadata: Metadata = {
   title: "Case ID : abc123 | Live Cases",
@@ -88,9 +91,7 @@ const page = ({params}: {params: { case: string }}) => {
                     <section className={paramedic__loc}>
                         <div role='heading' aria-level={2} className={general__title}><CiLocationOn fontSize={18} color="#215FFA"/><p>Paramedic Location</p></div>
                         <Suspense fallback={<MapLoadSkeleton/>}>
-                            <div className={paramedic__map}>
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11525.121488972181!2d78.96643550875214!3d16.010959768447595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb48c1241adb0e3%3A0x5c8ac521fa9f1f8c!2sNallamala%20Forest!5e1!3m2!1sen!2sin!4v1699970937012!5m2!1sen!2sin" style={{border: "0", width: '100%', height: 'auto', aspectRatio: '16/10', borderRadius: '12px' }} allowFullScreen loading="lazy"></iframe>
-                            </div>
+                            <Map/>
                         </Suspense>
                     </section>
 
