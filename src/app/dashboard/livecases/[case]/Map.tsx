8a27dc99cdefb070ce5dd9fd12d74a09a6bdbda3
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-11-16 12:31:22 +0530

    grid layout updated

--- src/app/dashboard/livecases/[case]/Map.tsx
+++ src/app/dashboard/livecases/[case]/Map.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 const Map = () => {
   return (
     <div className={styles.paramedic__map}>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11525.121488972181!2d78.96643550875214!3d16.010959768447595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb48c1241adb0e3%3A0x5c8ac521fa9f1f8c!2sNallamala%20Forest!5e1!3m2!1sen!2sin!4v1699970937012!5m2!1sen!2sin" style={{border: "0", width: '100%', height: 'auto', aspectRatio: '16/9', borderRadius: '12px' }} allowFullScreen loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.7754031622476!2d78.45028360915332!3d17.422562901725005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb91cb1dc49e8f%3A0xbab6cb3473be057d!2sNims%20Hospital!5e0!3m2!1sen!2sin!4v1700117291954!5m2!1sen!2sin" style={{border: "0", width: '100%', height: 'auto', aspectRatio: '16/9', borderRadius: '12px' }} allowFullScreen loading="lazy"></iframe>
     </div>
   )
 }
