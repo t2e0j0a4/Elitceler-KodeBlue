6efc3114af123cc4368963860d56779f3033818a
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-11-21 22:03:49 +0530

    changes in live case

--- src/app/dashboard/livecases/[case]/page.tsx
+++ src/app/dashboard/livecases/[case]/page.tsx
@@ -38,7 +38,6 @@ const page = ({params}: {params: { case: string }}) => {
                     <p>Live Cases</p>
                 </div>
                 <div className={head__talk}>
-                    <HeadButton label='Call' bgColor='#215FF2' />
                     <HeadButton label='Talk' bgColor='#215FF2' />
                     <HeadButton label='Documents' bgColor='#00CBFC' />
                     <HeadButton label='Assign' bgColor='#3CC35E' />
@@ -86,7 +85,7 @@ const page = ({params}: {params: { case: string }}) => {
                     
                     <section className={paramedic__loc}>
                         <div role='heading' aria-level={2} className={general__title}><CiLocationOn fontSize={18} color="#215FFA"/><p>Paramedic Location</p></div>
-                        <div className={styles.paramedic__map}>
+                        <div className={paramedic__map}>
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.7754031622476!2d78.45028360915332!3d17.422562901725005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb91cb1dc49e8f%3A0xbab6cb3473be057d!2sNims%20Hospital!5e0!3m2!1sen!2sin!4v1700117291954!5m2!1sen!2sin" style={{border: "0", width: '100%', height: 'auto', aspectRatio: '16/9', borderRadius: '12px' }} allowFullScreen loading="lazy"></iframe>
                         </div>
                     </section>
