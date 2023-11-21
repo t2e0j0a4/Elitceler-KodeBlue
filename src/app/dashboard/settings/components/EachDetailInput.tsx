97aeb44669e6a9b7bdb85cc2888ac2eccc03eefd
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-11-21 21:57:06 +0530

    updated settings

--- src/app/dashboard/settings/components/EachDetailInput.tsx
+++ src/app/dashboard/settings/components/EachDetailInput.tsx
@@ -6,15 +6,12 @@ import { BsPencilFill } from 'react-icons/bs';
 
 export default function EachDetailsInput ({label, placeholder, inputType = 'text'}: {label: string, placeholder: string, inputType?: 'email' | 'text'}) {
 
-    const {details__input, input__group} = styles;
+    const { input__group } = styles;
 
     return (
-        <div className={details__input}>
-            <div className={input__group}>
-                <label htmlFor={label.toLowerCase().replaceAll(' ','-')}>{label}</label>
-                <input type={inputType} placeholder={placeholder} id={label.toLowerCase().replaceAll(' ','-')} />
-            </div>
-            <button type='button' title="Edit"><BsPencilFill aria-hidden fontSize={19}/></button>
+        <div className={input__group}>
+            <label htmlFor={label.toLowerCase().replaceAll(' ','-')}>{label}</label>
+            <input type={inputType} placeholder={placeholder} id={label.toLowerCase().replaceAll(' ','-')} />
         </div>
     )
 }
