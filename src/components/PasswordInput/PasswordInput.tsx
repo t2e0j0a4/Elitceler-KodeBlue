1f83a00907984890e9221f5de38a74a826ee3aba
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-11-30 15:16:01 +0530

    added regexp validation to passwords

--- src/components/PasswordInput/PasswordInput.tsx
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -1,24 +1,40 @@
 'use client';
-import React, { useState } from 'react'
+import React, { use, useState } from 'react'
 import styles from "./PasswordInput.module.css";
 
 // React Icons
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { checkPasswordValidity } from '@/utils/CheckPassword';
 
 const PasswordInput = ({placeholder, label}: {placeholder: string, label: string}) => {
 
-    const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const [showPassError, setShowPassError] = useState<boolean>(true);
+  const isPasswordValid = (pass: string) => {
+    console.log(checkPasswordValidity(pass));
+    setShowPassError(checkPasswordValidity(pass));
+  }
 
   return (
-    <div className={styles.password__group}>
-        <input type={showPassword ? "text" : "password"} placeholder={placeholder} aria-labelledby={label} />
-        <button type='button' title={showPassword ? 'Hide' : 'Show'} onClick={() => {setShowPassword(!showPassword)}}>
-            {
+    <div className={styles.pass__withhelper}>
+      <div className={styles.password__group}>
+          <input type={showPassword ? "text" : "password"} placeholder={placeholder} aria-labelledby={label} onChange={(e) => {
+            isPasswordValid(e.target.value);
+          }} />
+          <button type='button' title={showPassword ? 'Hide' : 'Show'} onClick={() => {setShowPassword(!showPassword)}}>
+              {
                 showPassword ? 
                 <AiOutlineEyeInvisible fontSize={21}/> :
                 <AiOutlineEye fontSize={21}/>
-            }
-        </button>
+              }
+          </button>
+      </div>
+      {
+        showPassError || <p className={styles.show__helpertext}>
+          Password should 6-24 characters long, A uppercase, A lowercase, A digit, A special character.
+        </p>
+      }
     </div>
   )
 }
