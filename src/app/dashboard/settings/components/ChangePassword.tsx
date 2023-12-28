44d0876998fa71dab0eff07b06886eb7cb3bafd4
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-12-28 20:57:08 +0530

    update

--- src/app/dashboard/settings/components/ChangePassword.tsx
+++ src/app/dashboard/settings/components/ChangePassword.tsx
@@ -1,20 +1,34 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "../page.module.css";
 
 // Components
 import PasswordInput from '@/components/PasswordInput/PasswordInput';
 
 const ChangePassword = () => {
+
+  const [passwordDetails, setPasswordDetails] = useState({
+    currentPassword: '',
+    newPassword: '',
+    confirmNewPassword: ''
+  })
+
+  const updatePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordDetails({
+        ...passwordDetails,
+        [e.target.name]: e.target.value
+    });
+  }
+
   return (
     <form className={styles.change__password}>
         <h3>Change Your Password</h3>
-        <PasswordInput placeholder='Current Password' label='Current Password' />
-        <PasswordInput placeholder='New Password' label='New Password' />
-        <PasswordInput placeholder='Re-enter New Password' label='Re-enter New Password' />
+        <PasswordInput handleChange={updatePassword} value={passwordDetails.currentPassword} placeholder='Current Password' label='Current Password' />
+        <PasswordInput handleChange={updatePassword} value={passwordDetails.newPassword} placeholder='New Password' label='New Password' />
+        <PasswordInput handleChange={updatePassword} value={passwordDetails.confirmNewPassword} placeholder='Re-enter New Password' label='Re-enter New Password' />
         <button type='submit'>Update Password</button>
     </form>
   )
 }
 
-export default ChangePassword
+export default ChangePassword;
